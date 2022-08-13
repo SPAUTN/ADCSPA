5b1123bf8e9019a579362f834794d877b306e13b
julian87nicolas <julicmrgo@gmail.com>
2022-08-13 16:10:25 -0300

    pluviometro iniciado

--- ADC/ADC.ino
+++ ADC/ADC.ino
@@ -11,7 +11,7 @@
 #define Ccarga4 A9
 
 //------------- Definición entradas digitales -----------------------------
-#define SLluvia 50
+#define SLluvia 50 // TODO debe ser un pin de interrupción
 
 //------------ Variables para almacenar los datos -------------------------
 int Vviento = 0;
@@ -20,7 +20,7 @@ int Radiacion = 0;
 int Temperatura = 0;
 int HojaMojada = 0;
 int Humedad = 0;
-int Lluvia = 0;
+int contadorPluv = 0;
 int Celda1 = 0;
 int Celda2 = 0;
 int Celda3 = 0;
@@ -38,7 +38,7 @@ void setup() {
   pinMode(Ccarga2, INPUT);      // Entrada analógica
   pinMode(Ccarga3, INPUT);      // Entrada analógica
   pinMode(Ccarga4, INPUT);      // Entrada analógica
-  pinMode(SLluvia, INPUT);      // Entrada digital
+  attachInterrupt(digitalPinToInterrupt(SLluvia), cuentaPulsos, RISING); // Interrupción por flanco de subida
 }
 
 void loop() {
@@ -54,10 +54,14 @@ void loop() {
   float radiacion = setRadiacion(analogRead(SRad));
   float temperatura = setTemperatura(analogRead(STemperatura));
   String hojaMojada = setHoja(analogRead(SHMojada));
+  int lluvia = setLluvia(digitalRead(SLluvia));
 }
 
 /**
- * Setea si la hoja del campo esta mojada o seca
+ * @brief Setea si la hoja del campo esta mojada o seca
+ * 
+ * @param Hoja 
+ * @return String result indicando si la hoja está mojada o seca
  */
 String setHoja(int Hoja) {
   String resul = "";
@@ -74,16 +78,19 @@ String setHoja(int Hoja) {
 }
 
 /**
- * Setea la temperatura ambiente entre -25°C y 50°C
+ * @brief Setea la temperatura ambiente entre -25°C y 50°C
+ * 
+ * @param sensorTemp valor leido del sensor
+ * @return int temp valor real de temperatura
  */
-int setTemperatura(int T) {
+int setTemperatura(int sensorTemp) {
   float temp = 0;
-  switch (T)
+  switch (sensorTemp)
   {
-  case (T < 41):
+  case (sensorTemp < 41):
     temp = -25;
     break;
-  case (T < 41):
+  case (sensorTemp < 41):
     temp = (((T - 41) * 5 * 15) / 696) - 25;
     break;
   }
@@ -91,56 +98,68 @@ int setTemperatura(int T) {
 }
 
 /**
- * Setea la radiación solar entre 0 y 1400 W/m2
+ * @brief Setea la radiación solar entre 0 y 1400 W/m2
+ * 
+ * @param sensorRad valor leido del sensor de radiación solar
+ * @return int rad valor de radiacion solar
  */
-int setRadiacion(int R) {
+int setRadiacion(int sensorRad) {
   float rad = 0;
-  switch (R)
+  switch (sensorRad)
   {
-  case (R < 41):
+  case (sensorRad < 41):
     rad = 0;
     break;
-  case (R > 41):
-    rad = ((R - 41) * 5 * 280) / 716;
+  case (sensorRad > 41):
+    rad = ((sensorRad - 41) * 5 * 280) / 716;
     break;
   }
   return rad;
 }
 
 /**
- * Setea la humedad del ambiente entre 0% y 100%
+ * @brief Setea la humedad del ambiente entre 0% y 100%
+ * 
+ * @param sensorHum valor del sensor de humedad
+ * @return int humedad porcentaje de humedad
  */
-int setHumedad(int H) {
+int setHumedad(int sensorHum) {
   float humedad = 0;
-  switch (H)
+  switch (sensorHum)
   {
-  case (H < 102):
+  case (sensorHum < 102):
     humedad = 0;
-    break case (H > 102) : humedad = ((H - 102) * 5 * 20) / 716 break;
+    break;
+  case (sensorHum > 102) :
+    humedad = ((sensorHum - 102) * 5 * 20) / 716
+    break;
   }
   return humedad;
 }
 
 /**
- * Setea la dirección del viento
+ * @brief Setea la dirección del viento
+ * 
+ * @param sensorDir valor del sensor
+ * @return String direccionV que indica el nombre del punto cardinal donde apunta la dirección del viento
  */
-String setDireccionViento(int direccion) {
+String setDireccionViento(int sensorDir) {
   String direccionV = "";
-  switch (direccion)
+  switch (sensorDir)
   {
-  case (direccion > 39 && direccion < 103):
+  case (sensorDir > 39 && sensorDir < 103):
     direccionV = "NORTE";
     break;
-  case (direccion > 715 && direccion < 779):
+  case (sensorDir > 715 && sensorDir < 779):
     direccionV = "NORTE";
     break;
-  case (direccion > 306 && direccion < 431):
+  case (sensorDir > 306 && sensorDir < 431):
     direccionV = "SUR";
     break;
-  case (direccion > 122 && direccion < 247):
+  case (sensorDir > 122 && sensorDir < 247):
     direccionV = "ESTE";
     break;
-  case (direccion > 490 && direccion < 615):
+  case (sensorDir > 490 && sensorDir < 615):
     direccionV = "OESTE";
     break;
   }
@@ -148,8 +167,43 @@ String setDireccionViento(int direccion) {
 }
 
 /**
- * Convertimos a Km/h teniendo en cuenta que como maximo llegan 4V (818)(240km/h)
+ * @brief Calcula la velocidad del viento. Convertimos a Km/h teniendo en cuenta que
+ * como maximo llegan 4V (818)(240km/h)
+ * 
+ * @param sensorVel del sensor
+ * @return int valor de velocidad del viento
  */
-int setVelocidadViento(int lectura) {
-  return (lectura * 5 * 48) / 818;
+int setVelocidadViento(int sensorVel) {
+  return (sensorVel * 5 * 48) / 818;
+}
+
+/**
+ * @brief funcion ISR que aumenta el numero de pulsos al producirse
+ * una interrupcion por flanco de subida en el pin 
+ *
+ */
+void cuentaPulsos () {
+  contadorPluv++;
+}
+
+/**
+ * @brief Contador de pulsos digitales para pluviometro
+ * 
+ * @param contador cantidad de pulsos digitales
+ * @return int 
+ */
+int pluviometro (int contador) {
+  // Un valor habitual de Paso es 0,25 mm lo cual significa que por cada 1 mm de
+  // precipitación se producirán 4 vuelcos de cangilón
+    return contador*0.25;
+}
+
+/**
+ * @brief funcion para volver a cero el contador de pulsos del pluviometro
+ * debe determinarse si hacerlo cada cierto tiempo o mediante una señal
+ * externa de reset.
+ * 
+ */
+void resetContadorPluv () {
+  contadorPluv = 0;
 }
