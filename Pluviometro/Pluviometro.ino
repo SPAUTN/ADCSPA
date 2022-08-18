23b6832f8f922a8795bba98acc0b5ebe437b4ef8
Julian <julian.camargo@sitrack.com>
2022-08-18 12:47:02 -0300

    nombre modificado

--- Pluviometro/Pluviometro.ino
+++ Pluviometro/Pluviometro.ino
@@ -1,59 +1,59 @@
 #include <Time.h>
 #include <TimeLib.h>
 
-#define SLluvia 18     // pin digital que soporta interrupciones (validos: 2,3,18,19,20,21)
+#define PIN_PLUVIOMETRO 18     // pin digital que soporta interrupciones (validos: 2,3,18,19,20,21)
+#define TIME_THRESHOLD = 150
 
-const int timeThreshold = 150;
-volatile int contadorPluv = 0;
+volatile long int contadorPluv = 0;
 int pulsosXhora = 0;
 int pulsosXdia = 0;
 float precipDIA = 0;
 float precipHORA = 0;
 long startTime = 0;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   //Anotamos la hora y la fecha desde la que nuestro programa empezará a contar.
   //Formato: hora, minutos, segundos, días, mes, año
-  setTime(23,59,40,17,8,2022);
-  attachInterrupt(digitalPinToInterrupt(SLluvia), cuentaPulsos, RISING); // Interrupción por flanco de subida
+  setTime(23,59,40,31,8,2022);
+  attachInterrupt(digitalPinToInterrupt(PIN_PLUVIOMETRO), cuentaPulsos, RISING); // Interrupción por flanco de subida
 }
 
-void loop()
-{  
-     time_t t = now();//Declaramos la variable time_t 
-
-  if (hour(t)==23 && minute(t)==59 && second(t)==59){   // Para separar por dia.
-  pulsosXdia = contadorPluv;
-  precipDIA = 0.25*pulsosXdia;
-  Serial.print("Precipitaciones del dia: ");
-  Serial.print(day(t));
-  Serial.print(+ "/") ;
-  Serial.print(month(t));
-  Serial.print(+ "/") ;
-  Serial.println(year(t)); 
-  Serial.print(precipDIA);
-  Serial.println(" mm");
+void loop() {  
   
-  contadorPluv = 0;    //Reseteamos el contador de las interrupciones cada 24hs.
+  time_t t = now();//Declaramos la variable time_t 
+
+  if (hour(t)==23 && minute(t)==59 && second(t)==59) {   // Para separar por dia.
+    precipDIA = 0.25*pulsosXdia;
+    Serial.print("Precipitaciones del dia: ");
+    Serial.print(day(t));
+    Serial.print(+ "/") ;
+    Serial.print(month(t));
+    Serial.print(+ "/") ;
+    Serial.println(year(t)); 
+    Serial.print(precipDIA);
+    Serial.println(" mm");
+    pulsosXdia = 0;   // reseteamos el acumulado del dia
   }
-  if ( minute(t)==59 && second(t)==59){   // Para separar por hora.
-  pulsosXhora = contadorPluv;
-  precipHORA = 0.25*contadorPluv;
-  Serial.print("Precipitaciones a las: ");
-  Serial.print(hour(t));  
-  Serial.print(+ ":") ;
-  Serial.print(minute(t));
-  Serial.print(":") ;
-  Serial.println(second(t));
-  Serial.print(precipHORA);
-  Serial.println(" mm/h");
+
+   // CUIDADO POSIBLE PROBLEMA DE CONCURRENCIA (le quitamos un segundo a las horas)
+   
+  if ( minute(t)==59 && second(t)==58) {   // Para separar por hora.
+    pulsosXhora = contadorPluv;
+    pulsosXdia =  pulsosXdia + pulsosXhora;
+    precipHORA = 0.25 * pulsosXhora;
+    Serial.print("Precipitaciones durante las: ");
+    Serial.print(hour(t));  
+    Serial.println(+ "hs") ;
+    Serial.print(precipHORA);
+    Serial.println(" mm/h");
+
+    contadorPluv = 0;    //Reseteamos el contador de las interrupciones
   }
 }
 
 void cuentaPulsos(){
-  if (millis() - startTime > timeThreshold)   //anti rebote.
+  if (millis() - startTime > TIME_THRESHOLD)   //anti rebote.
   {  
     contadorPluv++;
     startTime = millis();
