7e70843924693a9b0d16a4b5aa36026328fab5dc
Julian <julian.camargo@sitrack.com>
2023-05-05 23:59:22 -0300

    listo poll

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #define TEST true     // true para modo test, sin espera de 1 minuto 
 #define TIME_THRESHOLD 150
+#define AT_CONTINUOUS_PRECV_CONFIG_SET "AT+PRECV=65534" 
 
 volatile static long int contadorPluv = 0;
 long startTime = 0;  //para anti rebote.
@@ -18,11 +19,12 @@ WeatherStation weatherStation;
 void setup() {
   Serial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, 19, 5);
-  sendATCommand(Serial1, AT_RESET);
-  sendATCommand(Serial1, AT_BAUD_115200_CONFIG_SET);
-  sendATCommand(Serial1, AT_P2P_CONFIG_SET);
-  sendATCommand(Serial1, AT_P2P_CONFIG_GET);
-  sendATCommand(Serial1, AT_P2P_CONFIG_TX_SET);
+  String response1 = sendATCommand(Serial1, AT_RESET);
+  String response2 = sendATCommand(Serial1, AT_BAUD_115200_CONFIG_SET);
+  String response3 = sendATCommand(Serial1, AT_P2P_CONFIG_SET);
+  String response4 = sendATCommand(Serial1, AT_P2P_CONFIG_GET);
+  String response5 = sendATCommand(Serial1, AT_CONTINUOUS_PRECV_CONFIG_SET);
+  //String response5 = sendATCommand(Serial1, AT_P2P_CONFIG_TX_SET);
 
   weatherStation.init();
 
@@ -35,30 +37,50 @@ void setup() {
 
   attachInterrupt(digitalPinToInterrupt(PLUVIOMETRO_PORT), pulseDetector, RISING); // Interrupción por flanco de subida
 
+  Serial.print("Response1: ");
+  Serial.println(response1);
+  Serial.print("Response2: ");
+  Serial.println(response2);
+  Serial.print("Response3: ");
+  Serial.println(response3);
+  Serial.print("Response4: ");
+  Serial.println(response4);
+  Serial.print("Response5: ");
+  Serial.println(response5);
+
   Serial.println("Setup finished");
 }
 
 void loop() {
-  time_t t = now();  
-  if ((second(t) == 30 && millis() - startTime > 1000) || TEST) {
-    weatherStation.setWindSpeed(analogRead(WIND_SPEED_SENSOR_PORT));       //se leen las entradas analogicas Estación meteorológica.
-    weatherStation.setwindDirection(analogRead(WIND_DIRECTION_SENSOR_PORT));
-    weatherStation.setHumidity(analogRead(HUMIDITY_SENSOR_PORT));
-    weatherStation.setRadiation(analogRead(RADIATION_SENSOR_PORT));
-    weatherStation.setTemperature();
-    weatherStation.setPresion();
-    weatherStation.setLeafMoisture(analogRead(LEAF_MOISTURE_SENSOR_PORT));
-    weatherStation.setPulseCounter(contadorPluv);
+  if(Serial1.available()>0) {
+    String rxData = readSerial(Serial1);
+    rxData.trim();
+    rxData = hexToASCII(rxData.substring(rxData.lastIndexOf(':')+1));
+    Serial.print("Instruction received: ");
+    Serial.println(rxData);
+    if (rxData.equals("POLL")) {
+      weatherStation.setWindSpeed(analogRead(WIND_SPEED_SENSOR_PORT));       //se leen las entradas analogicas Estación meteorológica.
+      weatherStation.setwindDirection(analogRead(WIND_DIRECTION_SENSOR_PORT));
+      weatherStation.setHumidity(analogRead(HUMIDITY_SENSOR_PORT));
+      weatherStation.setRadiation(analogRead(RADIATION_SENSOR_PORT));
+      weatherStation.setTemperature();
+      weatherStation.setPresion();
+      weatherStation.setLeafMoisture(analogRead(LEAF_MOISTURE_SENSOR_PORT));
+      weatherStation.setPulseCounter(contadorPluv);
 
-    Serial.print("Sending packet:");
-    Serial.println(weatherStation.getPayload());
-    String response = sendP2PPacket(Serial1, weatherStation.getPayload()); 
-    Serial.print("Response: ");
-    Serial.println(response);
+      Serial.print("Sending packet:");
+      Serial.println(weatherStation.getPayload());
+      sendATCommand(Serial1, AT_P2P_CONFIG_TX_SET);
+      String response = sendP2PPacket(Serial1, weatherStation.getPayload()); 
+      Serial.print("Response: ");
+      response.replace('\n', ' ');
+      Serial.println(response);
+      sendATCommand(Serial1, AT_CONTINUOUS_PRECV_CONFIG_SET);
 
-    contadorPluv = 0;
-    startTime = millis();
-    delay(3000);
+      contadorPluv = 0;
+      startTime = millis();
+      delay(3000);
+    }
   }
 }
 
